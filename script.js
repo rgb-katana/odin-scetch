767cf30bf83173c01f7a274bc62bc85122fff33b
rgb-katana <komarova1610@yandex.ru>
2022-11-22 19:33:58 +0300

    add lots of functionality with buttons now need to deal with mousedown events

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ const drawingWindow = document.querySelector('.drawing--window');
 const manipulationWindow = document.querySelector('.manipulation--window');
 const colors = document.querySelector('.colors');
 const colorNamer = document.querySelector('.color-name');
+const changeSqrBtn = document.querySelector('button');
+const eraserBtn = document.querySelector('.eraser--button');
+const clearBtn = document.querySelector('.clear--button');
+const blackBtn = document.querySelector('.black--button');
+const rainbowBtn = document.querySelector('.rainbow--button');
+const allButtons = document.querySelectorAll('.buttons');
 
 let sizeChoice = 16;
 let currentColor = 'black';
+let isColorSelected = false;
+colorNamer.innerText = 'black';
+colorNamer.style.color = 'white';
+colorNamer.classList = [];
+colorNamer.classList.add('black');
 
 updateSquaresSize(sizeChoice);
 
@@ -32,7 +43,6 @@ function resizeSquares(diseredSize) {
 }
 
 drawingWindow.addEventListener('mouseover', function (e) {
-  console.log(e.target);
   e.target.style.backgroundColor = `${currentColor}`;
 });
 
@@ -41,7 +51,7 @@ window.addEventListener('resize', function () {
 });
 
 colors.addEventListener('mouseover', function (e) {
-  if (e.target.classList.contains('color')) {
+  if (e.target.classList.contains('color') && !isColorSelected) {
     colorNamer.classList = [];
     colorNamer.innerText = e.target.classList[1];
     colorNamer.style.color = 'white';
@@ -51,9 +61,65 @@ colors.addEventListener('mouseover', function (e) {
 });
 
 colors.addEventListener('mouseout', function (e) {
-  if (e.target.classList.contains('color')) {
+  if (e.target.classList.contains('color') && !isColorSelected) {
     colorNamer.classList = [];
     colorNamer.classList.add('color-name');
     colorNamer.innerText = ' ';
   }
+});
+
+changeSqrBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  sizeChoice = document.getElementById('change-squares').value;
+  document.getElementById('change-squares').value = '';
+  updateSquaresSize(sizeChoice);
+});
+
+colors.addEventListener('click', function (e) {
+  if (e.target.classList.contains('color')) {
+    console.log(e.target.dataset.clr);
+    currentColor = e.target.dataset.clr;
+    colorNamer.classList = [];
+    colorNamer.innerText = e.target.classList[1];
+    colorNamer.style.color = 'white';
+    colorNamer.style.textShadow = `1px 1px 2px black`;
+    colorNamer.classList.add(e.target.classList[1]);
+    isColorSelected = true;
+    allButtons.forEach(function (btn) {
+      btn.classList.remove('pressed');
+    });
+  }
+});
+
+eraserBtn.addEventListener('click', function (e) {
+  allButtons.forEach(function (btn) {
+    btn.classList.remove('pressed');
+  });
+  e.target.classList.add('pressed');
+  currentColor = 'white';
+  colorNamer.classList = [];
+  colorNamer.innerText = 'white';
+  colorNamer.style.color = 'black';
+  colorNamer.style.textShadow = `1px 1px 2px black`;
+});
+
+clearBtn.addEventListener('click', function (e) {
+  allButtons.forEach(function (btn) {
+    btn.classList.remove('pressed');
+  });
+  e.target.classList.add('pressed');
+  updateSquaresSize(sizeChoice);
+  setTimeout(() => e.target.classList.remove('pressed'), 100);
+});
+
+blackBtn.addEventListener('click', function (e) {
+  allButtons.forEach(function (btn) {
+    btn.classList.remove('pressed');
+  });
+  e.target.classList.add('pressed');
+  currentColor = 'black';
+  colorNamer.classList = [];
+  colorNamer.classList.add('black');
+  colorNamer.innerText = 'black';
+  colorNamer.style.color = 'white';
 });
