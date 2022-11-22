20e5d47aaac6cecded34368f08f9d68e62772949
KirMac <komarova1610@yandex.ru>
2022-11-22 17:01:58 +0300

    added hovering effects to display color names; added resizing scripts; added black button

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const drawingWindow = document.querySelector('.drawing--window');
 const manipulationWindow = document.querySelector('.manipulation--window');
+const colors = document.querySelector('.colors');
+const colorNamer = document.querySelector('.color-name');
 
-function placeSquares(diseredSize) {
+let sizeChoice = 16;
+let currentColor = 'black';
+
+updateSquaresSize(sizeChoice);
+
+function updateSquaresSize(diseredSize) {
+  drawingWindow.innerHTML = '';
   sqrsPixels = drawingWindow.getBoundingClientRect().width / diseredSize;
   for (let i = 0; i < diseredSize ** 2; i++) {
     const square = document.createElement('div');
+    square.classList.add(`square`);
     square.classList.add(`square${i}`);
     square.style.width = `${sqrsPixels}px`;
     square.style.height = `${sqrsPixels}px`;
@@ -13,8 +22,38 @@ function placeSquares(diseredSize) {
   }
 }
 
-placeSquares(16);
+function resizeSquares(diseredSize) {
+  const allSquares = document.querySelectorAll(`.square`);
+  allSquares.forEach((square) => {
+    sqrsPixels = drawingWindow.getBoundingClientRect().width / diseredSize;
+    square.style.width = `${sqrsPixels}px`;
+    square.style.height = `${sqrsPixels}px`;
+  });
+}
 
 drawingWindow.addEventListener('mouseover', function (e) {
-  e.target.style.backgroundColor = 'black';
+  console.log(e.target);
+  e.target.style.backgroundColor = `${currentColor}`;
+});
+
+window.addEventListener('resize', function () {
+  resizeSquares(sizeChoice);
+});
+
+colors.addEventListener('mouseover', function (e) {
+  if (e.target.classList.contains('color')) {
+    colorNamer.classList = [];
+    colorNamer.innerText = e.target.classList[1];
+    colorNamer.style.color = 'white';
+    colorNamer.style.textShadow = `1px 1px 2px black`;
+    colorNamer.classList.add(e.target.classList[1]);
+  }
+});
+
+colors.addEventListener('mouseout', function (e) {
+  if (e.target.classList.contains('color')) {
+    colorNamer.classList = [];
+    colorNamer.classList.add('color-name');
+    colorNamer.innerText = ' ';
+  }
 });
